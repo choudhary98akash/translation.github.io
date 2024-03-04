bb0f18def55d5ce25f964121490c8950ceaad41b
Akash Choudhary <choudahry98akash>
2024-03-04 06:29:57 +0530

    First Page ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'translator';
+  home = true;
+  faq = false;
+  feedback = false;
+  howto = false;
+  contact = false;
+
+
+
+
+
+  showHome() {
+    this.home = true;
+    this.faq = false;
+    this.feedback = false;
+    this.howto = false;
+    this.contact = false;
+  }
+  showFeedBack() {
+    this.home = false;
+    this.faq = false;
+    this.feedback = true;
+    this.howto = false;
+    this.contact = false;
+  }
+  showContact() {
+    this.home = false;
+    this.faq = false;
+    this.feedback = false;
+    this.howto = false;
+    this.contact = true;
+  }
+  showFaq() {
+    this.home = false;
+    this.faq = true;
+    this.feedback = false;
+    this.howto = false;
+    this.contact = false;
+  }
+  showHOwTo() {
+    this.home = false;
+    this.faq = false;
+    this.feedback = false;
+    this.howto = true;
+    this.contact = false;
+  }
 }
+
+
